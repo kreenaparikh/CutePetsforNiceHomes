881c3b75d328712cc4293fb0f170f04ec502f593
kreenaparikh <parikhkreena@gmail.com>
2017-05-06 10:20:19 -0700

    Initial Commit

---
+++ Cute Pets for Nice Homes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cute Pets for Nice Homes
+//
+//  Created by Kreena Parikh on 5/6/17.
+//  Copyright © 2017 Kreena Parikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
